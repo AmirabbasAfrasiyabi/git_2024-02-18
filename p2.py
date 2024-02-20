091b1205f5ecc7b1e2a19e1c980048b1717a736e
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-02-20 23:38:26 +0330

    changes p2.py and added project in this file

---
+++ p2.py
@@ -0,0 +1,13 @@
+# project2
+
+a = int(input("Enter a : " ))
+b = int(input("Enter b : " ))
+
+#process
+Result = None
+if ( a > b) :
+    Result = a
+else:
+    Result = b
+#out put
+print( " the Result is " , Result)
