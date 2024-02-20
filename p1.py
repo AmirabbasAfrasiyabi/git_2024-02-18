a0b7864a72d2180c95530b56e593179cf88cb510
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-02-20 23:12:08 +0330

    add p1 in project

---
+++ p1.py
@@ -0,0 +1,12 @@
+# input
+number = int(input( " please enter number : " ))
+
+# process
+Sum = None
+Sum = 0
+for counter in range ( 1 , number + 1 , 1) :
+    if ( number % counter == 0 ) :
+        Sum = Sum + counter
+# out put        
+print ( Sum)
+ 
