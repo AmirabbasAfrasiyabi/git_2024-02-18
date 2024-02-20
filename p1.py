46ad0b496f4b9e01d6e49b8aee697a3a8f2c2370
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-02-20 23:19:45 +0330

    project2 created in p2

--- p1.py
+++ p1.py
@@ -12,16 +12,18 @@
 # # out put        
 # print ( Sum)
  
+
+
 # project2
 
-a = int(input("Enter a : " ))
-b = int(input("Enter b : " ))
+# a = int(input("Enter a : " ))
+# b = int(input("Enter b : " ))
 
-#process
-Result = None
-if ( a > b) :
-    Result = a
-else:
-    Result = b
-#out put
-print( " the Result is " , Result)
+# #process
+# Result = None
+# if ( a > b) :
+#     Result = a
+# else:
+#     Result = b
+# #out put
+# print( " the Result is " , Result)
