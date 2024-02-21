3849f9dc0dec68167bc52c3be3c329c24677e962
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-02-21 16:17:44 +0330

    update project 1

--- python/p1.py
+++ python/p1.py
@@ -1,13 +1,13 @@
 
-###project1
-# # input
-# number = int(input( " please enter number : " ))
+##project1
+# input
+number = int(input( " please enter number : " ))
 
-# # process
-# Sum = None
-# Sum = 0
-# for counter in range ( 1 , number + 1 , 1) :
-#     if ( number % counter == 0 ) :
-#         Sum = Sum + counter
-# # out put        
-# print ( Sum)
+# process
+Sum = None
+Sum = 0
+for counter in range ( 1 , number + 1 , 1) :
+    if ( number % counter == 0 ) :
+        Sum = Sum + counter
+# out put        
+print ( Sum)
